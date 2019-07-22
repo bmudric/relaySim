a891e82e7c6310f88b5f1b7290b2b50e39852176
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2019-07-22 22:08:06 +0200

    refactor the random status controller

--- src/main/java/com/jazavac/relaysim/controller/RandomStatusController.java
+++ src/main/java/com/jazavac/relaysim/controller/RandomStatusController.java
@@ -4,11 +4,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/status")
-public class StatusController {
+/** This class is used to simulate getting a status of a relay that turns on and off every second */
+@RestController
+public class RandomStatusController {
 
-  @GetMapping
-  public String returnStatus(HttpServletResponse response){
+  @GetMapping("/randomstatus")
+  public String returnStatus(HttpServletResponse response) {
     response.addHeader("Access-Control-Allow-Origin", "*");
     response.addHeader("Content-Type", "application/json");
     return String.format("{\"relayIsOn\":%b}", generateStatus());
