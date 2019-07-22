34aedce129768660abd6f9fd6d80990557778efa
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2019-07-23 01:38:27 +0200

    add mDNS service registration

--- src/main/java/com/jazavac/relaysim/RelaySimApplication.java
+++ src/main/java/com/jazavac/relaysim/RelaySimApplication.java
@@ -1,13 +1,25 @@
 package com.jazavac.relaysim;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RelaySimApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+    ServiceInfo serviceInfo =
+        ServiceInfo.create(
+            "_relayAPI._tcp.local.",
+            "Relay API simulator",
+            8080,
+            "GET /status; POST /on; POST /off");
+    jmdns.registerService(serviceInfo);
+
     SpringApplication.run(RelaySimApplication.class, args);
   }
-
 }
