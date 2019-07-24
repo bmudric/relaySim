a860134df5ca3ea83f961eece0ffa2cd438affb6
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2019-07-24 18:13:54 +0200

    add service mDNS registration on Spring ApplicationStartedEvent

--- src/main/java/com/jazavac/relaysim/discovery/ServiceRegistrator.java
+++ src/main/java/com/jazavac/relaysim/discovery/ServiceRegistrator.java
@@ -1,37 +1,48 @@
 package com.jazavac.relaysim.discovery;
 
+import static java.lang.Integer.parseInt;
+
 import java.io.IOException;
 import java.net.InetAddress;
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ServiceRegistrator {
 
   private static final String SERVICE_TYPE = "_relayAPI._tcp.local.";
   private static final String SERVICE_NAME = "relay_API_simulator";
   private static final String SERVICE_TEXT = "GET /status; POST /on; POST /off";
-  private static final int SERVICE_PORT = 8080; // TODO get port at runtime
 
   private JmDNS jmDNS;
 
-  @PostConstruct
-  void registerService() throws IOException {
+  private Environment environment;
+
+  public ServiceRegistrator(Environment env) {
+    this.environment = env;
+  }
+
+  @EventListener(ApplicationStartedEvent.class)
+  public void registerService() throws IOException {
+    int runningPort = parseInt(environment.getProperty("local.server.port"));
     jmDNS = JmDNS.create(InetAddress.getLocalHost());
     ServiceInfo serviceInfo =
-        ServiceInfo
-            .create(SERVICE_TYPE, SERVICE_NAME, SERVICE_PORT, SERVICE_TEXT);
+        ServiceInfo.create(SERVICE_TYPE, SERVICE_NAME, runningPort, SERVICE_TEXT);
     jmDNS.registerService(serviceInfo);
-    System.out.println("Service registered");
+    log.debug("Service registered");
   }
 
   @PreDestroy
   void unregisterService() throws IOException {
     jmDNS.unregisterAllServices();
     jmDNS.close();
-    System.out.println("Service unregistered");
+    log.debug("Service unregistered");
   }
 }
